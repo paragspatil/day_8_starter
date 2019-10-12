83af8d63c5deccfaa6e35c4f79ec786dcc7541ca
Parag <paragspatil123@gmail.com>
2019-10-12 11:45:58 +0530

    BMI day 9 complete

--- lib/input.dart
+++ lib/input.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:day_8_starter/main.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-
-
 
 class InputPage extends StatefulWidget {
   @override
@@ -11,115 +7,188 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  int gender; // Male = 0, Female = 1
+  Color activeCardColor =  Color(0xFF1D1F31);
+  Color inactiveCardColor =  Color(0xFF111328);
+  Color activetextColor = Colors.white;
+  Color inactivetextColor = Colors.white70;
+  double _sliderValue = 183.0;
   @override
   Widget build(BuildContext context) {
+    Color textcolormale = inactivetextColor;
+    Color textcolorfemale = inactivetextColor;
+    Color backgroundColorMale = inactiveCardColor;
+    Color backgroundColorFemale = inactiveCardColor;
+
+    if (gender == 0) {
+      backgroundColorMale = activeCardColor;
+      textcolormale = activetextColor;
+    } else if (gender == 1) {
+      backgroundColorFemale = activeCardColor;
+      textcolorfemale = activetextColor;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
+        centerTitle: true,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ResuseableCard(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          Icon(FontAwesomeIcons.mars,  size: 120,),
-                          Text('MALE', style: TextStyle(fontSize: 32),)
-                        ],
-                      ),
+                  child: ReusableCard(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 100,
+                          color:  textcolormale,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(fontSize: 24,
+                            color: textcolormale,
+                          ),
+                        )
+                      ],
                     ),
+                    color: backgroundColorMale,
+                    tapCallback: () {
+                      setState(() {
+                        gender = 0;
+                      });
+                      print('Male selected');
+                    },
                   ),
                 ),
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ResuseableCard(
-                    child:  Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          Icon(FontAwesomeIcons.venus,  size: 120,),
-                          Text('FEMALE', style: TextStyle(fontSize: 32),)
-                        ],
-                      ),
+                  child: ReusableCard(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.venus,
+                          size: 100,
+                            color: textcolorfemale,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'FEMALE',
+                          style: TextStyle(fontSize: 24,
+                              color: textcolorfemale,
+                          ),
+                        )
+                      ],
                     ),
+                    color: backgroundColorFemale,
+                    tapCallback: () {
+                      setState(() {
+                        gender = 1;
+                      });
+
+                      print('Female selected');
+                    },
                   ),
-                ),
-
-
+                )
               ],
             ),
           ),
           Expanded(
-            child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ResuseableCard(),
+            child: ReusableCard(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Text(
+                'HEIGHT',
+                 style: TextStyle(fontSize: 24),
+                 textAlign: TextAlign.center,
+              ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.baseline,
+                 textBaseline: TextBaseline.alphabetic,
+                 children: <Widget>[
+                   Text(
+                     '${_sliderValue.toInt()}',
+                     style: TextStyle(
+                       fontSize: 42, fontWeight: FontWeight.bold)
+
+                   ),
+                   Text(
+                     'cm', style: TextStyle(
+                     fontSize: 24,
+                   ),
+                   )
+
+                 ],
+               ),
+                  Slider(
+                    activeColor: Colors.pink,
+                    inactiveColor: Colors.white,
+                    min: 110.0,
+                    max: 210.0,
+                    onChanged: (newRating) {
+                      setState(() => _sliderValue = newRating);
+                    },
+                    value: _sliderValue,
                   ),
-                ),
-              ],
+                ],
+
+              ),
             ),
           ),
           Expanded(
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ResuseableCard(),
-                  ),
+                  child: ReusableCard(),
                 ),
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ResuseableCard(),
-                  ),
-                ),
+                  child: ReusableCard(),
+                )
               ],
-
             ),
           ),
-
-
         ],
-      )
-
+      ),
     );
   }
 }
 
-class ResuseableCard extends StatelessWidget {
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  final Widget child;
+  final Function tapCallback;
 
- final Widget child;
- final Color color;
- ResuseableCard({this.color = const Color(0xFF1D1F31), this.child});
+  ReusableCard(
+      {this.color = const Color(0xFF1D1F31), this.child, this.tapCallback});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius:BorderRadius.all(Radius.circular(12)),
-        color: color,
+    return GestureDetector(
+      onTap: tapCallback,
+      child: Container(
+        margin: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.all(
+            Radius.circular(12),
+          ),
+        ),
+        child: child,
       ),
-
-      child: child,
     );
   }
 }
